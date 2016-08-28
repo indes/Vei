8eb9e96066e5f3abb375714c090c3c2d67f42ed6
indes <wanglicsly@gmail.com>
2016-08-28 15:37:37 +0800

    集成Smarty模板引擎

---
+++ core/app/config/view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: li
+ * Date: 2016-08-28
+ * Time: 15:10
+ */
---
+++ core/lib/VIEW.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: li
+ * Date: 2016-08-28
+ * Time: 15:06
+ */
+
+
+class VIEW
+{
+    static public $view;
+    static function init(){
+        
+    }
+}
